f93cb83d7e7a7e73cbf6b3f777933024d961d795
unknown <you@example.com>
2022-01-29 00:49:12 +0400

    first commit

---
+++ HomeWorkApp/Controllers/CarController.cs
@@ -0,0 +1,38 @@
+﻿using HomeWorkApp.DAL;
+using HomeWorkApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWorkApp.Controllers
+{
+    public class CarController: Controller
+    {
+        private readonly FirstDbContext firstdb;
+
+        public CarController(FirstDbContext _firstdb)
+        {
+            firstdb = _firstdb;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View(firstdb.Cars);
+        }
+
+        public IActionResult Info(int id)
+        {
+            Car car = firstdb.Cars.FirstOrDefault(x => x.Id == id);
+            if(car== null)
+            {
+                TempData["error"] = "Car doesnt exit.";
+
+            }
+            return View(car);
+
+        }
+    }
+}
---
+++ HomeWorkApp/DAL/FirstDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HomeWorkApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWorkApp.DAL
+{
+    public class FirstDbContext : DbContext
+
+    {
+        public FirstDbContext(DbContextOptions<FirstDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
